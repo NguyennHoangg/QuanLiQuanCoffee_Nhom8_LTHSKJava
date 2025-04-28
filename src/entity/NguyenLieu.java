9c94b62b101bbdc07bb4c2c0136d7e9383b88d78
unknown <tranthihaiyen210805@gmail.com>
2025-04-28 13:33:10 +0700

    Quản Lý Giao Diện Nhân Viên, Phiếu Nhập

--- src/entity/NguyenLieu.java
+++ src/entity/NguyenLieu.java
@@ -1,16 +1,25 @@
 package entity;
 
+import java.sql.Date;
+
 public class NguyenLieu {
     private String maNguyenLieu;
     private String tenNguyenLieu;
-    private String donViTinh; // Ví dụ: kg, lít, cái
-    private double giaNhap; // Giá nhập nguyên liệu
+    private String donViTinh;
+    private double giaNhap;
+    private Date ngayNhap;
+    private Date NgayHetHan;
+    private KhoNguyenLieu khoNguyenLieu;
 
-    public NguyenLieu(String maNguyenLieu, String tenNguyenLieu, String donViTinh, double giaNhap) {
+    //contructor
+    public NguyenLieu(String maNguyenLieu, String tenNguyenLieu, String donViTinh, double giaNhap, Date ngayNhap, Date ngayHetHan, KhoNguyenLieu khoNguyenLieu) {
         this.maNguyenLieu = maNguyenLieu;
         this.tenNguyenLieu = tenNguyenLieu;
         this.donViTinh = donViTinh;
         this.giaNhap = giaNhap;
+        this.ngayNhap = ngayNhap;
+        this.NgayHetHan = ngayHetHan;
+        this.khoNguyenLieu = khoNguyenLieu;
     }
 
     public String getMaNguyenLieu() {
@@ -54,13 +63,39 @@ public class NguyenLieu {
         this.giaNhap = giaNhap;
     }
 
+    public Date getNgayNhap() {
+        return ngayNhap;
+    }
+
+    public void setNgayNhap(Date ngayNhap) {
+        this.ngayNhap = ngayNhap;
+    }
+
+    public Date getNgayHetHan() {
+        return NgayHetHan;
+    }
+
+    public void setNgayHetHan(Date ngayHetHan) {
+        NgayHetHan = ngayHetHan;
+    }
+
+    public void setKhoNguyenLieu(KhoNguyenLieu khoNguyenLieu) {
+        this.khoNguyenLieu = khoNguyenLieu;
+    }
+
+    public KhoNguyenLieu getKhoNguyenLieu() {
+        return khoNguyenLieu;
+    }
     @Override
     public String toString() {
         return "NguyenLieu{" +
                 "maNguyenLieu='" + maNguyenLieu + '\'' +
                 ", tenNguyenLieu='" + tenNguyenLieu + '\'' +
                 ", donViTinh='" + donViTinh + '\'' +
-                ", giaNhap=" + giaNhap +
+                ", giaNhap=" + giaNhap + '\'' +
+                ", ngayNhap=" + ngayNhap + '\'' +
+                ", NgayHetHan=" + NgayHetHan + '\'' +
+                ", khoNguyenLieu=" + khoNguyenLieu +
                 '}';
     }
 }
